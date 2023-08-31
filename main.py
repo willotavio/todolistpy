6643a57e395043b7d83c29083ac4cca16ff43bec
willotavio <willaotavio@gmail.com>
2023-08-31 14:51:36 -0300

    Add delete method

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ def add_task():
         todo_list.insert(0, task_entry.get())
         task_entry.delete(0, tk.END)
 
+def delete_task():
+    if todo_list.curselection():
+        todo_list.delete(todo_list.curselection())
+
 window = tk.Tk()
 window.title("To-Do")
+window.geometry("420x420")
 
 todo_list = tk.Listbox(window)
-todo_list.pack()
+todo_list.pack(pady=10)
 
 task_entry = tk.Entry(window)
-task_entry.pack()
+task_entry.pack(pady=10)
 
 submit = tk.Button(window, text="Submit", command=add_task)
-submit.pack()
+submit.pack(pady=10)
+
+delete = tk.Button(window, text="Delete", command=delete_task)
+delete.pack(pady=5)
 
 window.mainloop()
 
