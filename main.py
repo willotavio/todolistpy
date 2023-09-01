f8152a2332ff7b3afd8b792c7c6cf6c56a748124
willotavio <willaotavio@gmail.com>
2023-09-01 19:59:49 -0300

    Fix update method

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def add_task():
 def delete_task():
     if todo_list.curselection():
         todo_list.delete(todo_list.curselection())
+        if len(id_entry.get()):
+            id_entry.delete(0)
 
-def update_task():
-    id_entry.delete(0, tk.END)
-    task_entry.delete(0, tk.END)
-    if todo_list.curselection():
-        id_entry.insert(0, todo_list.curselection())
-        task_entry.insert(0, todo_list.get(todo_list.curselection()))
+def update_task(event):
+    if event.widget.curselection():
+        id_entry.delete(0)
+        id_entry.insert(0, event.widget.curselection())
 
 window = tk.Tk()
 window.title("To-Do")
@@ -28,6 +28,8 @@ window.geometry("420x420")
 
 todo_list = tk.Listbox(window)
 todo_list.pack(pady=10)
+
+todo_list.bind("<<ListboxSelect>>", update_task)
 
 id_entry = tk.Entry(window)
 id_entry.pack(pady=10)
@@ -39,9 +41,6 @@ submit.pack(pady=10)
 
 delete = tk.Button(window, text="Delete", command=delete_task)
 delete.pack(pady=5)
-
-update = tk.Button(window, text="Update", command=update_task)
-update.pack(pady=5)
 
 window.mainloop()
 
