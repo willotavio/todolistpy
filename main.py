b418da8e3849f8a54e4d0ff305c4cbaf4c2354d8
willotavio <willaotavio@gmail.com>
2023-09-02 11:04:04 -0300

    Add tabs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def add_task():
     if len(task_entry.get()) > 0:
@@ -15,6 +16,7 @@ def delete_task():
     if len(current_id.get()) > 0:
         todo_list.delete(id_entry.get())
         current_id.set("")
+        task_entry.delete(0, tk.END)
 
 def select_task(event):
     task_entry.delete(0, tk.END)
@@ -33,36 +35,46 @@ def mark_complete():
 
 window = tk.Tk()
 window.title("To-Do")
-window.geometry("420x720")
+window.geometry("420x520")
 
-todo_list_title = tk.Label(window, text="To-Do List")
+notebook = ttk.Notebook(window)
+
+todo_list_tab = ttk.Frame(notebook)
+notebook.add(todo_list_tab, text="To-Do List")
+
+finished_list_tab = ttk.Frame(notebook)
+notebook.add(finished_list_tab, text="Finished List")
+
+notebook.pack()
+
+todo_list_title = tk.Label(todo_list_tab, text="To-Do List")
 todo_list_title.pack()
-todo_list = tk.Listbox(window)
+todo_list = tk.Listbox(todo_list_tab)
 todo_list.pack(pady=10)
 
 todo_list.bind("<<ListboxSelect>>", select_task)
 todo_list.bind("<Double-1>", deselect_task)
 
-finished_list_title = tk.Label(window, text="Finished List")
-finished_list_title.pack()
-finished_list = tk.Listbox(window)
-finished_list.pack(pady=10)
-
 current_id = tk.StringVar()
 
-id_entry = tk.Entry(window, textvariable=current_id, state=tk.DISABLED)
+id_entry = tk.Entry(todo_list_tab, textvariable=current_id, state=tk.DISABLED)
 id_entry.pack(pady=10)
 
-task_entry = tk.Entry(window)
+task_entry = tk.Entry(todo_list_tab)
 task_entry.pack(pady=10)
 
-submit = tk.Button(window, text="Submit", command=add_task)
+submit = tk.Button(todo_list_tab, text="Submit", command=add_task)
 submit.pack(pady=10)
 
-delete = tk.Button(window, text="Delete", command=delete_task)
+delete = tk.Button(todo_list_tab, text="Delete", command=delete_task)
 delete.pack(pady=5)
 
-complete = tk.Button(window, text="Complete", command=mark_complete)
+complete = tk.Button(todo_list_tab, text="Complete", command=mark_complete)
 complete.pack(pady=5)
+
+finished_list_title = tk.Label(finished_list_tab, text="Finished List")
+finished_list_title.pack()
+finished_list = tk.Listbox(finished_list_tab)
+finished_list.pack(pady=10)
 
 window.mainloop()
